b581becdb1c2442933622ac1937d4b117c5f77b7
KATTIRISETTY <abhishek.kattirisetty@xcubelabs.com>
2020-08-11 13:06:32 +0530

    Added Aspect Logging

--- src/main/java/com/example/genericrestapi/aspect/LoggingAspect.java
+++ src/main/java/com/example/genericrestapi/aspect/LoggingAspect.java
@@ -1,33 +1,33 @@
-//package com.example.genericrestapi.aspect;
-//
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//@Aspect
-//@Component
-//public class LoggingAspect {
-//
-//	private final Logger log = LoggerFactory.getLogger(this.getClass());
-//
-//	@Around(value = "execution(* com.example.genericrestapi.*.*.*(..))" + "&& !within(com.example.genericrestapi.config.*)")
-//	public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-//
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		String methodName = proceedingJoinPoint.getSignature().getName();
-//		String className = proceedingJoinPoint.getTarget().getClass().toString();
-//		Object[] arguments = proceedingJoinPoint.getArgs();
-//		log.info("Call from " + className + ":" + methodName + "()" + "Arguments :"
-//				+ objectMapper.writeValueAsString(arguments));
-//		Object response = proceedingJoinPoint.proceed();
-//		log.info("Response from " + className + ":" + methodName + "()" + "Response :"
-//				+ objectMapper.writeValueAsString(response));
-//
-//		return response;
-//	}
-//}
+package com.example.genericrestapi.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Around(value = "execution(* com.example.genericrestapi.*.*.*(..))" + "&& !within(com.example.genericrestapi.config.*)"+ "&& !within(com.example.genericrestapi.factory.*)")
+	public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String methodName = proceedingJoinPoint.getSignature().getName();
+		String className = proceedingJoinPoint.getTarget().getClass().toString();
+		Object[] arguments = proceedingJoinPoint.getArgs();
+		log.info("Call from " + className + ":" + methodName + "()" + "Arguments :"
+				+ objectMapper.writeValueAsString(arguments));
+		Object response = proceedingJoinPoint.proceed();
+		log.info("Response from " + className + ":" + methodName + "()" + "Response :"
+				+ objectMapper.writeValueAsString(response));
+
+		return response;
+	}
+}
