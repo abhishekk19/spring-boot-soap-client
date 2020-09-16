8042cf76bf1271f2a98443fe6bd03f10e90abc17
Abhishek <abhishek.kattirisetty@xcubelabs.com>
2020-09-16 17:02:43 +0530

    Updating request for Apis

---
+++ src/main/java/com/example/genericrestapi/request/PhleboSlotsRequest.java
@@ -0,0 +1,17 @@
+package com.example.genericrestapi.request;
+
+public class PhleboSlotsRequest {
+
+	public String from_date;
+	public String to_date;
+	public int engagement_level;
+	public String role_id;
+	public String patient_gender;
+	public String service_did;
+	public String pin_code;
+	public int patient_age;
+	public int patient_id;
+	public String order_latitude;
+	public String order_longitude;
+
+}
---
+++ src/main/java/com/example/genericrestapi/request/RescheduleOrderRequest.java
@@ -0,0 +1,14 @@
+package com.example.genericrestapi.request;
+
+public class RescheduleOrderRequest {
+
+	public String order_id;
+	public String actionById;
+	public String actionByName;
+	public String reason;
+	public String scheduled_date;
+	public String start_time;
+	public String end_time;
+	public long chiss_transaction_id;
+
+}
